25e6971f07f09b2e79e154c2a2a3c320c0286153
Daniel Miller <dm635@cornell.edu>
2016-11-15 21:54:34 -0500

    encapsulated

--- 2016-11-15_Kolmogorov-Smirnov/recipes.py
+++ 2016-11-15_Kolmogorov-Smirnov/recipes.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import scipy.stats.kstest as kstest
+from scipy.stats import kstest
 
 def from_lpdata(name):
     filename = '%s_lpdata.txt' % name
     df = pd.from_csv(filename)
 
-for i in xrange(1,67):
-    num = 10000*i
-    plt.clf()
-    plt.hist(df.head(num).K, bins=200, normed=True)
-    plt.xlabel('First %d primes' % num)
-    plt.savefig('%d.png' % i)
+def histograms(df, heads):
+    for h in heads:
+        plt.clf()
+	plt.hist(df.head(h).K, bins=200, normed=True)
+	plt.xlabel(str(h))
+	plt.savefig('%d.png' % h)
+
