45f138b894094aac6e2636250259af140fc25f44
dkmiller <daniel.keegan.miller@gmail.com>
2018-05-12 10:31:20 -0700

    need to fully expane ch:ver -> verse_num

--- 2018/2018-05-12_ryan-analysis.R
+++ 2018/2018-05-12_ryan-analysis.R
@@ -1,3 +1,11 @@
 library(tidyverse)
 
-pain.sayings <- read_csv('pain.verses.csv')
+chapter.lengths <- read_csv('2018-05-12_chapter-lengths.csv', skip=1) %>%
+  mutate(chapter = row_number())
+
+pain.sayings <- read_csv('2018-05-12_pain-verses.csv', col_names = FALSE) %>%
+  separate(X1, c('chapter', 'verse'), ':') %>%
+  mutate(chapter = as.numeric(chapter), verse = as.numeric(verse))
+
+pain.sayings %>%
+  left_join(chapter.lengths, by = 'chapter')
