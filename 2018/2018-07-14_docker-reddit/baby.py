45783bc9a1216d57326bb4492f59690fb99877e6
Daniel Miller <daniel.keegan.miller@gmail.com>
2018-07-17 19:06:23 -0700

    remove client id from praw.ini

--- 2018/2018-07-14_docker-reddit/baby.py
+++ 2018/2018-07-14_docker-reddit/baby.py
@@ -7,7 +7,7 @@ import praw
 
 def main(args):
     print('Howdy!')
-    reddit = praw.Reddit('baby', client_secret=args.secret)
+    reddit = praw.Reddit('baby',client_id=args.id, client_secret=args.secret)
     subreddit = reddit.subreddit('learnpython')
 
     for submission in subreddit.hot(limit=5):
@@ -15,7 +15,8 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the baby Reddit bot.')
-    parser.add_argument('--secret', help='Client secret.', required=True)
+    parser.add_argument('--id', help='Client ID', required=True)
+    parser.add_argument('--secret', help='Client secret', required=True)
     args = parser.parse_args()
     main(args)
 
