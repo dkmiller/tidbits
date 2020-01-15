e6d3a248d89073a91edd8327078c0b74e0eedd90
Daniel Miller <daniel.keegan.miller@gmail.com>
2020-01-14 21:01:07 -0800

    all tests passing

--- 2020/2020-01-14_flask/src/ml.py
+++ 2020/2020-01-14_flask/src/ml.py
@@ -3,8 +3,18 @@ All ML and data related code.
 '''
 
 import io
+import json
+import lazy_object_proxy as lazy
 import torchvision.transforms as transforms
 from PIL import Image
+from torchvision import models
+
+def _init_model():
+    model = models.densenet121(pretrained=True)
+    model.eval()
+    return model
+
+_model = lazy.Proxy(_init_model)
 
 _transforms = transforms.Compose([
     transforms.Resize(255),
@@ -14,6 +24,12 @@ _transforms = transforms.Compose([
         [0.485, 0.456, 0.406],
         [0.229, 0.224, 0.225])
 ])
+
+def get_prediction(bytes):
+    tensor = transform_image(bytes)
+    outputs = _model.forward(tensor)
+    _, ŷ = outputs.max(1)
+    return ŷ
 
 def transform_image(bytes):
     '''
--- 2020/2020-01-14_flask/test/test_ml.py
+++ 2020/2020-01-14_flask/test/test_ml.py
@@ -6,13 +6,24 @@ from urllib import request
 # https://stackoverflow.com/a/27876800
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
-from src.ml import transform_image
+from src.ml import get_prediction, transform_image
+
+def get_bytes(url):
+    response = request.urlopen(url)
+    return response.read()
+
+@pytest.mark.parametrize('postfix,class_id', [('/773/200/200', '980')])
+def test_get_prediction(postfix, class_id):
+    url = f'https://picsum.photos/id/{postfix}.jpg'
+    data = get_bytes(url)
+    ŷ = get_prediction(data)
+    ŷ = str(ŷ.item())
+    assert ŷ == class_id
 
 @pytest.mark.parametrize('postfix', ['300', '800', '/100/300'])
 def test_transform_image_on_small_image(postfix):
     url = f'https://picsum.photos/{postfix}'
-    response = request.urlopen(url)
-    data = response.read()
+    data = get_bytes(url)
     tensor = transform_image(data)
     assert len(tensor) == 1
     assert tensor.shape[0] == 1
