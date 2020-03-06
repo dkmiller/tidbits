d6dd32c927ca2b20f64e370928772367cd37f382
Dan Miller <danmill@microsoft.com>
2020-03-06 23:11:42 +0000

    trying to deploy

--- 2020/2020-03-06_azureml/3.a/deploy.py
+++ 2020/2020-03-06_azureml/3.a/deploy.py
@@ -1,8 +1,9 @@
 import argparse
 import os
-from azureml.core import Experiment, Workspace
+from azureml.core import Experiment, Workspace, Webservice
 from azureml.core.model import InferenceConfig, Model
 from azureml.core.webservice import AciWebservice
+from azureml.exceptions import WebserviceException
 
 
 def main(args, ws):
@@ -15,9 +16,17 @@ def main(args, ws):
     dc = AciWebservice.deploy_configuration(cpu_cores=args.cores, memory_gb=args.memory)
     m = ws.models[args.model_name]
 
+    # Remove any existing service under the same name.
+    try:
+        Webservice(ws, args.service_name).delete()
+        print(f'Deleted webservice with name {args.service_name}.')
+    except WebserviceException:
+        pass
+
     service = Model.deploy(ws, args.service_name, [m], ic, dc)
     service.wait_for_deployment(True)
     print(service.state)
+    print(service.get_logs())
 
 
 if __name__ == '__main__':
@@ -25,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--cores', default=1, type=int)
     parser.add_argument('--memory', default=1, type=int)
     parser.add_argument('--model_name', default='safe_driver_prediction')
-    parser.add_argument('--service_name', default='danmill-safe-driver-2')
+    parser.add_argument('--service_name', default='danmill-safe-driver')
 
     args = parser.parse_args()
     ws = Workspace.from_config()
