1d57f639bddbb8167da951648b1ca6ec07651132
Dan Miller <danmill@microsoft.com>
2020-03-16 16:48:22 -0700

    factored model definition + methods into standalone file

--- 2020/2020-03-13_pytorch/test/test_training.py
+++ 2020/2020-03-13_pytorch/test/test_training.py
@@ -1,58 +1,24 @@
 import pytest
 import torch
 from torch.autograd import Variable
-from torch.distributions.normal import Normal
 import src.seed
 from src.func import compose
+from src.models.linear_model import LinearModel, linear, mock_data
 
 
 def setup_module():
     src.seed.set_seed()
 
 
-class LinearModel(torch.nn.Module):
-    '''
-    Neural network transforming a specified number of inputs into a single
-    number via a linear transformation.
-    '''
-
-    def __init__(self, input_size):
-        super(LinearModel, self).__init__()
-        self.linear = torch.nn.Linear(input_size, 1, True)
-
-    def forward(self, x):
-        return self.linear(x)
-
-
-def _linear(weights, x, bias):
-    return torch.dot(weights, x) + bias
-
-
-def _linear_data(weights, bias, N, σ):
-    size = len(weights)
-    noise = Normal(torch.zeros(1), torch.Tensor([float(σ)]))
-    xs = []
-    ys = []
-    for _ in range(N):
-        x = torch.rand(size)
-        ε = noise.sample()
-        y = _linear(weights, x, bias) + ε
-        xs.append(x)
-        ys.append(y)
-    xs = torch.stack(xs)
-    ys = torch.cat(ys).view(-1, 1)
-    return (xs, ys)
-
-
 @pytest.mark.parametrize('weights,bias,N,σ', [
     ([1, 2, 3], -5, 100, 1)
 ])
 def test_can_create_linear_data(weights, bias, N, σ):
-    (xs, ys) = _linear_data(torch.Tensor(weights), bias, N, σ)
+    (xs, ys) = mock_data(torch.Tensor(weights), bias, N, σ)
     assert N == len(xs)
     assert N == len(ys)
-    assert all (type(x) == torch.Tensor for x in xs)
-    assert all (type(y) == torch.Tensor for y in ys)
+    assert all(type(x) == torch.Tensor for x in xs)
+    assert all(type(y) == torch.Tensor for y in ys)
 
 
 @pytest.mark.parametrize('weights,bias,N,σ,ε,lr,epochs', [
@@ -61,7 +27,7 @@ def test_can_create_linear_data(weights, bias, N, σ):
 ])
 def test_training_converges_to_linear_model(weights, bias, N, σ, ε, lr, epochs):
     weights = torch.Tensor(weights)
-    (xs, ys) = _linear_data(weights, bias, N, σ)
+    (xs, ys) = mock_data(weights, bias, N, σ)
 
     model = LinearModel(len(weights))
     criterion = torch.nn.MSELoss()
@@ -77,7 +43,7 @@ def test_training_converges_to_linear_model(weights, bias, N, σ, ε, lr, epochs
         optimizer.step()
 
     x = torch.ones(len(weights))
-    y = _linear(weights, x, bias).item()
+    y = linear(weights, x, bias).item()
     ŷ = model(x).item()
 
     assert ŷ == pytest.approx(y, ε)
