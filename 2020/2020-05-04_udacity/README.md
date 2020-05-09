941ef1fda058a81a4648902a9e21d3b091efb4b6
Dan Miller <danmill@microsoft.com>
2020-05-08 20:21:41 -0700

    pause

--- 2020/2020-05-04_udacity/README.md
+++ 2020/2020-05-04_udacity/README.md
@@ -4,3 +4,7 @@
 
 - [Titanic](https://www.openml.org/d/40945)
 - [How do I get the full path of the current file's directory?](https://stackoverflow.com/a/3430395)
+- [LightGBM / python-guide](https://github.com/microsoft/LightGBM/blob/master/examples/python-guide/simple_example.py)
+- [How do I create test and train samples from one dataframe with pandas?](https://stackoverflow.com/a/24151789)
+- [`typing` &mdash; Support for type hints](https://docs.python.org/3/library/typing.html)
+- [?](https://towardsdatascience.com/predicting-the-survival-of-titanic-passengers-30870ccc7e8)
--- 2020/2020-05-04_udacity/src/titanic.py
+++ 2020/2020-05-04_udacity/src/titanic.py
@@ -1,4 +1,26 @@
+import pandas as pd
 from src import common
 
-def titanic_csv():
-    return common.path_in_data_directory('titanic.csv', 'https://www.openml.org/data/get_csv/16826755/phpMYEkMl')
+
+def sex_as_int(sex: str) -> str:
+    if sex == 'male':
+        return 1
+    return 0
+
+
+def prepare_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(columns=['cabin', 'home.dest', 'name', 'ticket'])
+    df['sex'] = df.apply(lambda r: sex_as_int(r['sex']))
+    return df
+
+
+def titanic_csv() -> str:
+    return common.path_in_data_directory(
+        'titanic.csv',
+        'https://www.openml.org/data/get_csv/16826755/phpMYEkMl'
+    )
+
+
+def raw_data() -> pd.DataFrame:
+    csv = titanic_csv()
+    return pd.read_csv(csv)
