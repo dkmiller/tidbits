1cb2c5dec5076fff8c47db020938a5b8083ac531
Dan Miller <danmill@microsoft.com>
2020-06-08 13:31:00 -0700

    call azure service bus

---
+++ 2020/2020-06-08_azure-service-bus/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dkmiller.AzureServiceBus.Client
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="connectionString">Azure Service Bus connection string.</param>
+        /// <param name="maxConcurrentCalls">Maximum number of concurrent message receives.</param>
+        /// <param name="numMessagesSend">Send this many messages.</param>
+        /// <param name="queueName"> Azure Service Bus queue name.</param>
+        static async Task Main(
+            string connectionString,
+            string queueName = "test_queue",
+            int numMessagesSend = 10,
+            int maxConcurrentCalls = 2)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new QueueClient(connectionString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(e =>
+            {
+                Console.WriteLine(e);
+                return Task.CompletedTask;
+            })
+            {
+                MaxConcurrentCalls = maxConcurrentCalls,
+                AutoComplete = false
+            };
+
+            client.RegisterMessageHandler(async (message, _) =>
+            {
+                Console.WriteLine($"Received message (seq # {message.SystemProperties.SequenceNumber}): '{Encoding.Default.GetString(message.Body)}'.");
+                await client.CompleteAsync(message.SystemProperties.LockToken);
+            },
+            messageHandlerOptions);
+
+            await SendMessages(numMessagesSend, client);
+
+            Console.WriteLine("Enter key to finish.");
+            Console.ReadKey();
+
+            await client.CloseAsync();
+        }
+
+        private static async Task SendMessages(int numMessages, IQueueClient client)
+        {
+            for (var i = 0; i < numMessages; ++i)
+            {
+                var body = $"Message {i}";
+                var message = new Message(Encoding.Default.GetBytes(body));
+                Console.WriteLine($"Sending message: '{body}'.");
+                await client.SendAsync(message);
+            }
+        }
+    }
+}
---
+++ 2020/2020-06-08_azure-service-bus/README.md
@@ -0,0 +1,15 @@
+# Azure Service Bus
+
+Follow
+[Get started with Service Bus queues](https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues).
+
+```powershell
+dotnet run --connection-string '<conn string here>'
+```
+
+## Links
+
+- [Parse the Command Line with `System.CommandLine`](https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/march/net-parse-the-command-line-with-system-commandline)
+- [?](https://mitchdenny.com/the-magic-of-system-commandline/)
+- [?](https://github.com/dotnet/command-line-api/wiki/Your-first-app-with-System.CommandLine.DragonFruit)
+- https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/november/csharp-iterating-with-async-enumerables-in-csharp-8
