2554740d9c2782e3883ebf095ccccf1af78b26c7
Dan Miller <danmill@microsoft.com>
2020-06-09 18:06:15 -0700

    first commit: build passing

---
+++ 2020/2020-06-09_orleans/README.md
@@ -0,0 +1,9 @@
+# Orleans
+
+https://dotnet.github.io/orleans/index.html
+
+## Links
+
+- [Download .NET 5](https://dotnet.microsoft.com/download/dotnet/5.0)
+- [Tutorial One - Creating a Minimal Orleans Application](http://dotnet.github.io/orleans/Documentation/tutorials_and_samples/tutorial_1.html)
+- [?](https://aka.ms/msbuild/developerpacks/)
---
+++ 2020/2020-06-09_orleans/Silo/Arithmetic.cs
@@ -0,0 +1,25 @@
+﻿using Orleans;
+using dkmiller.Core;
+using Microsoft.Extensions.Logging;
+using Open.Numeric.Primes.Extensions;
+using System.Threading.Tasks;
+
+
+namespace dkmiller.Silo
+{
+    public class Arithmetic : Grain, IArithmetic
+    {
+        private ILogger Logger { get; }
+
+        public Arithmetic(ILogger<Arithmetic> logger) =>
+            Logger = logger;
+        
+        public Task<long> NextPrime(long index)
+        {
+            Logger.LogInformation($"Calculating {nameof(NextPrime)}({index}).");
+            var result = index.NextPrime();
+            Logger.LogInformation($"The next prime is {result}.");
+            return Task.FromResult(result);
+        }
+    }
+}
