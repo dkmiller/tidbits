d412b2956b52e3427a821e3145fbd49a84966f7e
Daniel Miller <daniel.keegan.miller@gmail.com>
2020-12-16 21:18:14 -0800

    add skeleton code

---
+++ 2020/2020-12-15_graph-algorithms/Graph.Algorithms/AsyncBreadthFirstSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Graph.Algorithms
+{
+    public class AsyncBreadthFirstSearch<T> : IAsyncAlgorithm<T>
+        where T : IAsyncNode<T>
+    {
+        public Task VisitAsync(T node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
