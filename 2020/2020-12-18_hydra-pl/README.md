940d7c499376a9854f030b9932c35b8f4c339412
Dan Miller <danmill@microsoft.com>
2020-12-21 09:32:32 -0800

    Submitted run 2e267a1d-8349-4cf3-9b68-cfd74204d57d

--- 2020/2020-12-18_hydra-pl/README.md
+++ 2020/2020-12-18_hydra-pl/README.md
@@ -15,6 +15,16 @@ First time without the `data.download=...`.
 python train.py data.root=/src/tmp/data data.download=false
 ```
 
+# Running in AML
+
+```
+git clean -xdf; az ml job create --file job.yml --query metadata.interaction_endpoints.studio --out tsv
+```
+
+TODO: ...
+
 ## Links
 
 - [Lightning in 2 steps](https://pytorch-lightning.readthedocs.io/en/stable/new-project.html)
+- [Train models (create jobs)](https://azure.github.io/azureml-v2-preview/_build/html/quickstart/jobs.html)
+- [Distributed training](https://azure.github.io/azureml-v2-preview/_build/html/quickstart/distributed-training.html)
--- 2020/2020-12-18_hydra-pl/train.py
+++ 2020/2020-12-18_hydra-pl/train.py
@@ -1,6 +1,10 @@
+from typing import Union
+from azureml.core import Run
+from azureml.core.run import _SubmittedRun
 import hydra
 import logging
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import MLFlowLogger, LightningLoggerBase
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
@@ -9,17 +13,37 @@ from torchvision.transforms import ToTensor
 from ml import AutoEncoder
 
 
+def mlflow_logger() -> Union[bool, LightningLoggerBase]:
+    """
+    Loosely imitate:
+    https://github.com/Azure/azureml-examples/blob/main/tutorials/using-pytorch-lightning/3.log-with-mlflow.ipynb
+    """
+    run = Run.get_context()
+    if isinstance(run, _SubmittedRun):
+        tracking_uri = run.experiment.workspace.get_mlflow_tracking_uri()
+        exp_name = run.id
+        rv = MLFlowLogger(exp_name, tracking_uri)
+    else:
+        rv = True
+
+    return rv
+
+
 @hydra.main(config_name="config")
 def main(config):
     log = logging.getLogger(__name__)
     log.info(f"Configuration: {config}")
+
+    pl.seed_everything(config.seed)
 
     model = AutoEncoder(**config.model)
 
     dataset = MNIST(transform=ToTensor(), **config.data)
     train_loader = DataLoader(dataset)
 
-    trainer = pl.Trainer(**config.trainer)
+    logger = mlflow_logger()
+
+    trainer = pl.Trainer(logger, **config.trainer)
     trainer.fit(model, train_loader)
 
 
