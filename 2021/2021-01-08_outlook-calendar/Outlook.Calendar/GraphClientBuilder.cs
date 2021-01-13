bf62430f790ec4b205136be011824aa47849ec76
Dan Miller <danmill@microsoft.com>
2021-01-13 15:13:43 -0800

    add some scopes

--- 2021/2021-01-08_outlook-calendar/Outlook.Calendar/GraphClientBuilder.cs
+++ 2021/2021-01-08_outlook-calendar/Outlook.Calendar/GraphClientBuilder.cs
@@ -2,6 +2,7 @@
 using Azure.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
+using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
@@ -11,12 +12,26 @@ namespace Outlook.Calendar
     {
         private DefaultAzureCredential Credential { get; } = new();
 
+        private const string Scopes = "Calendars.ReadWrite Contacts.ReadWrite DeviceManagementApps.ReadWrite.All DeviceManagementConfiguration.Read.All DeviceManagementConfiguration.ReadWrite.All DeviceManagementManagedDevices.PrivilegedOperations.All DeviceManagementManagedDevices.Read.All DeviceManagementManagedDevices.ReadWrite.All DeviceManagementRBAC.Read.All DeviceManagementRBAC.ReadWrite.All DeviceManagementServiceConfig.Read.All DeviceManagementServiceConfig.ReadWrite.All Directory.AccessAsUser.All Directory.ReadWrite.All Files.ReadWrite.All Group.ReadWrite.All IdentityRiskEvent.Read.All Mail.ReadWrite MailboxSettings.ReadWrite Notes.ReadWrite.All openid People.Read Presence.Read Presence.Read.All profile Reports.Read.All Sites.ReadWrite.All Tasks.ReadWrite User.Read User.ReadBasic.All User.ReadWrite User.ReadWrite.All email";
+
         private async Task<string> GetAccessTokenAsync()
         {
             Logger.LogInformation("Obtaining access token using Azure credential.");
+
+            // TODO: getting unauthorized exception.
+            // Not all scopes are respected :( .
+            // Get this list by pasting access token into https://jwt.ms and looking under `scp`.
+            var scopes = Scopes.Split(" ")
+                .Select(scope => $"https://graph.microsoft.com/{scope}")
+                .ToArray();
+            //var scopes = new[]
+            //{
+            //    "https://graph.microsoft.com/Calendars.ReadWrite",
+            //    "https://graph.microsoft.com/Contacts.ReadWrite",
+            //    "https://graph.microsoft.com/User.ReadWrite.All",
+            //};
             var token = await Credential.GetTokenAsync(
-                // TODO: getting unauthorized exception.
-                new TokenRequestContext(new[] { "https://graph.microsoft.com/Calendars.ReadWrite" })
+                new TokenRequestContext(scopes)
                 );
 
             var accessToken = token.Token;
