fbaf24c2345a7c73ba0d9a8d24a814d7e825d7e8
Dan Miller <danmill@microsoft.com>
2021-01-14 09:00:27 -0800

    somehow things are working now...

--- 2020/2020-12-18_hydra-pl/submit.py
+++ 2020/2020-12-18_hydra-pl/submit.py
@@ -7,25 +7,31 @@ https://azure.github.io/azureml-web/docs/cheatsheet/script-run-config
 
 
 from azureml.core import Environment, Experiment, ScriptRunConfig, Workspace
+from azureml.core.runconfig import MpiConfiguration
 
 
 ws = Workspace("48bbc269-ce89-4f6f-9a12-c6f91fcb772d", "aml1p-rg", "aml1p-ml-wus2")
 
 env = Environment.from_conda_specification("hydra-pl", "environment.yml")
+env.docker.enabled = True
 env.docker.base_image = (
-    "mcr.microsoft.com/azureml/openmpi3.1.2-cuda10.2-cudnn7-ubuntu18.04"
+    "mcr.microsoft.com/azureml/openmpi3.1.2-cuda10.1-cudnn7-ubuntu18.04"
 )
+
+mpi_config = MpiConfiguration(process_count_per_node=1, node_count=1)
+
 
 config = ScriptRunConfig(
     source_directory=".",
     script="train.py",
     compute_target="gpu-nc12-lowpri",
+    distributed_job_config=mpi_config,
     environment=env,
     arguments=["trainer.gpus=1", "trainer.num_nodes=1"],
 )
 
 exp = Experiment(ws, "azuremlv2")
 
-run = exp.submit(config, tags={"from": "sdkv1"})
+run = exp.submit(config, tags={"from": "sdkv1", "docker.enabled": "True", "cuda": "10.1", "pl": "pip"})
 
 print(run.get_portal_url())
