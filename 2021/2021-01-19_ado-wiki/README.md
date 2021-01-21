2df21d29c00d1df120407414056dd07ce3c3a0b7
Daniel Miller <daniel.keegan.miller@gmail.com>
2021-01-20 19:59:05 -0800

    can get stats, just need to format

--- 2021/2021-01-19_ado-wiki/README.md
+++ 2021/2021-01-19_ado-wiki/README.md
@@ -4,3 +4,4 @@
 
 - [Xpath like query for nested python dictionaries](https://stackoverflow.com/a/26050704)
 - [How to programmatically list wiki pages with Azure DevOps Service rest API](https://stackoverflow.com/a/57086334)
+- https://docs.microsoft.com/en-us/rest/api/azure/devops/wiki/pages%20batch/get?view=azure-devops-rest-6.0
--- 2021/2021-01-19_ado-wiki/page-views.py
+++ 2021/2021-01-19_ado-wiki/page-views.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+from typing import Dict, List
 from azure.identity import DefaultAzureCredential
 import hydra
 import jmespath
@@ -16,26 +16,51 @@ def get_auth_headers(*scopes: List[str]) -> Dict[str, str]:
     return headers
 
 
+def get_wiki_id(base_url: str, wiki_name: str, headers: Dict[str, str]) -> str:
+    """
+    Not necessary, but good use of JMESPath.
+    """
+    url = f"{base_url}/_apis/wiki/wikis"
+    r = requests.get(url, headers=headers)
+    wikis = r.json()
+    wiki_id = jmespath.search(f"value[?name == '{wiki_name}'].id", wikis)[0]
+
+    return wiki_id
+
+
+def get_all_page_stats(
+    base_url: str, wiki_name: str, headers: Dict[str, str], top: int = 100
+):
+    url = (
+        f"{base_url}/_apis/wiki/wikis/{wiki_name}/pagesbatch?api-version=6.0-preview.1"
+    )
+    body = {"top": top, "pageViewsForDays": 30}
+    r = requests.post(url, headers=headers, json=body)
+    json = r.json()
+
+    print(json)
+    # TODO: handle pagination.
+
+    pages = json["value"]
+    for page in pages:
+        path = page["path"]
+        log.info(path)
+        for stat in page["viewStats"]:
+            print(stat)
+        print(page)
+
+
 @hydra.main(config_name="config")
 def main(config):
     log.info(f"Configuration: {config}")
 
     headers = get_auth_headers(*config.scopes)
 
-    r = requests.get(f"{config.url}/_apis/wiki/wikis", headers=headers)
-    wikis = r.json()
-    wiki_id = jmespath.search(f"value[?name == '{config.wiki}'].id", wikis)[0]
+    wiki_id = get_wiki_id(config.url, config.wiki, headers=headers)
+    log.info(f"ID of wiki {config.wiki} = {wiki_id}")
 
-    r = requests.get(f"{config.url}/_apis/wiki/wikis/{wiki_id}/pages?path=/Home&api-version=6.0", headers=headers)
-    page_id = r.json()["id"]
-
-    # https://dev.azure.com/msdata/Vienna/_apis/wiki/wikis/0003dacc-7379-491b-901d-416322f17283/pages?recursionLevel=full&api-version=6.0
-
-    # With wiki name vs. ID:
-    # https://dev.azure.com/msdata/Vienna/_apis/wiki/wikis/aml-1p-onboarding/pages?recursionLevel=full&api-version=6.0
-
-    # page view stats...
-    # https://dev.azure.com/msdata/Vienna/_apis/wiki/wikis/0003dacc-7379-491b-901d-416322f17283/pages/11188/stats?pageViewsForDays=30
+    stats = get_all_page_stats(config.url, config.wiki, headers)
+    log.info(f"Page view stats: {stats}")
 
 
 if __name__ == "__main__":
