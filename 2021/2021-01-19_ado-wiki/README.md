b15e07a72962e1107ebdc791964e073b287ea62b
Daniel Miller <daniel.keegan.miller@gmail.com>
2021-01-22 10:04:31 -0800

    Call Azure DevOps REST API to get wiki page view analytics (#54)
    * analytics for ADO wiki
    
    * can get stats, just need to format
    
    * works with paging, even against large wiki

---
+++ 2021/2021-01-19_ado-wiki/README.md
@@ -0,0 +1,14 @@
+# Azure DevOps Wiki page view APIs
+
+To run against a "big" wiki, use:
+
+```powershell
+python page-views.py url=https://dev.azure.com/experimentation/Analysis%20and%20Experimentation wiki=AnE.wiki
+```
+
+## Links
+
+- [Xpath like query for nested python dictionaries](https://stackoverflow.com/a/26050704)
+- [How to programmatically list wiki pages with Azure DevOps Service rest API](https://stackoverflow.com/a/57086334)
+- [Azure DevOps &mdash; Pages Batch - Get](https://docs.microsoft.com/en-us/rest/api/azure/devops/wiki/pages%20batch/get?view=azure-devops-rest-6.0)
+- [Pretty-print an entire Pandas Series / DataFrame](https://stackoverflow.com/a/39923958)
---
+++ 2021/2021-01-19_ado-wiki/page-views.py
@@ -0,0 +1,102 @@
+from typing import Dict, List
+from azure.identity import DefaultAzureCredential
+import hydra
+import jmespath
+import logging
+import pandas as pd
+import requests
+
+
+log = logging.getLogger(__name__)
+
+
+def get_auth_headers(*scopes: List[str]) -> Dict[str, str]:
+    credential = DefaultAzureCredential()
+    access_token = credential.get_token(*scopes).token
+    headers = {"Authorization": f"Bearer {access_token}"}
+    return headers
+
+
+def get_wiki_id(base_url: str, wiki_name: str, headers: Dict[str, str]) -> str:
+    """
+    Not necessary, but good use of JMESPath.
+    """
+    url = f"{base_url}/_apis/wiki/wikis"
+    r = requests.get(url, headers=headers)
+    wikis = r.json()
+    wiki_id = jmespath.search(f"value[?name == '{wiki_name}'].id", wikis)[0]
+
+    return wiki_id
+
+
+def get_pages_batch(
+    base_url: str, wiki_name: str, json: dict, headers: Dict[str, str]
+) -> requests.Response:
+    url = (
+        f"{base_url}/_apis/wiki/wikis/{wiki_name}/pagesbatch?api-version=6.0-preview.1"
+    )
+    log.info(f"Calling POST {url} with body {json}")
+    rv = requests.post(url, headers=headers, json=json)
+    return rv
+
+
+def get_all_page_stats(
+    base_url: str, wiki_name: str, headers: Dict[str, str], top: int = 100
+) -> pd.DataFrame:
+
+    count = None
+    continuation_token = None
+    body = {"top": top, "pageViewsForDays": 30}
+    pages = []
+
+    while count != 0:
+        if continuation_token is not None:
+            body["continuationToken"] = continuation_token
+
+        r = get_pages_batch(base_url, wiki_name, body, headers)
+        json = r.json()
+        new_pages = json["value"]
+        pages.extend(new_pages)
+        count = json["count"]
+        continuation_token = r.headers.get("x-ms-continuationtoken")
+        log.info(f"Got {len(new_pages)} pages.")
+
+    d = {"day": [], "path": [], "count": []}
+
+    for page in pages:
+        path = page["path"]
+        if "viewStats" in page:
+            for stat in page["viewStats"]:
+                d["day"].append(stat["day"])
+                d["path"].append(path)
+                d["count"].append(stat["count"])
+        else:
+            log.warning(f"Page {path} does not have view stats.")
+
+    rv = pd.DataFrame(d)
+    return rv
+
+
+@hydra.main(config_name="config")
+def main(config):
+    log.info(f"Configuration: {config}")
+
+    headers = get_auth_headers(*config.scopes)
+
+    wiki_id = get_wiki_id(config.url, config.wiki, headers=headers)
+    log.info(f"ID of wiki {config.wiki} = {wiki_id}")
+
+    stats = get_all_page_stats(config.url, config.wiki, headers)
+    log.info(f"Page view stats: {stats}")
+
+    agg_stats = (
+        stats.groupby(["path"])["count"]
+        .agg("sum")
+        .sort_values(ascending=False)
+        .head(30)
+    )
+    log.info(f"Aggregated stats: {agg_stats.to_string()}")
+
+
+if __name__ == "__main__":
+    main()
