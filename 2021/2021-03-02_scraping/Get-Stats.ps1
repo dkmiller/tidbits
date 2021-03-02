e2e12316adac65c6b8d359b1a74137c25357bc62
Dan Miller <danmill@microsoft.com>
2021-03-02 14:20:22 -0800

    put inside try / except

--- 2021/2021-03-02_scraping/Get-Stats.ps1
+++ 2021/2021-03-02_scraping/Get-Stats.ps1
@@ -1,5 +1,6 @@
 param(
-    $File
+    $File,
+    [int]$Nonce
 )
 
 $Data = import-csv $File
@@ -10,12 +11,17 @@ $NewData = New-Object System.Collections.Generic.List[System.Object]
 foreach ($Line in $Data) {
     $Id = $Line.ShortUrl.Split("=")[-1]
     write-host $Id
-    $Resp = iwr "https://marriageheat.com/wp-admin/admin-ajax.php" -Method Post -Body "action=load_results&postID=$Id&nonce=41908004f0"
-    $Json = $Resp.Content | convertfrom-json
-    write-host $Json
-    $Line | Add-Member -NotePropertyName VoteCount -NotePropertyValue $Json.voteCount
-    $Line | Add-Member -NotePropertyName AvgRating -NotePropertyValue $Json.avgRating
-    $NewData.Add($Line)
+    try {
+        $Resp = iwr "https://marriageheat.com/wp-admin/admin-ajax.php" -Method Post -Body "action=load_results&postID=$Id&nonce=$Nonce"
+        $Json = $Resp.Content | convertfrom-json
+        write-host $Json
+        $Line | Add-Member -NotePropertyName VoteCount -NotePropertyValue $Json.voteCount
+        $Line | Add-Member -NotePropertyName AvgRating -NotePropertyValue $Json.avgRating
+        $NewData.Add($Line)
+    
+    } catch {
+        Write-Warning "failed :("
+    }
 }
 
 $NewData | Export-Csv new-data.csv
