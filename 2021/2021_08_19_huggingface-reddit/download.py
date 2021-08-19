bb6c116d0a102ed6358642541232819e28c5ac47
Dan Miller <daniel.keegan.miller@gmail.com>
2021-08-19 16:35:45 +0000

    start code for downloading training & eval data

--- 2021/2021_08_19_huggingface-reddit/download.py
+++ 2021/2021_08_19_huggingface-reddit/download.py
@@ -1,5 +1,6 @@
 import argparse
 import praw
+from sklearn.model_selection import train_test_split
 
 
 def emphasize(o):
@@ -14,6 +15,7 @@ def get_argument_parser():
     parser.add_argument("--client_secret", required=True, type=str)
     parser.add_argument("--post_limit", default=10, type=int)
     parser.add_argument("--subreddit", required=True, type=str)
+    parser.add_argument("--train_size", default=0.9, type=float)
     parser.add_argument("--user_agent", default="praw.ml")
     return parser
 
@@ -26,9 +28,17 @@ def get_reddit_client(args) -> praw.Reddit:
 def main(args):
     reddit = get_reddit_client(args)
     posts = reddit.subreddit(args.subreddit).top("all", limit=args.post_limit)
-    for post in posts:
-        emphasize(post.title)
-        print(f"\n\n{post.selftext}\n\n")
+    dataset = [f"<|title|> {post.selftext.strip()}" for post in posts]
+    train, eval = train_test_split(dataset, train_size=args.train_size)
+    print(f"Train, evaluation size: {len(train)}, {len(eval)}")
+
+    with open(".temp/train.txt", "w") as f:
+        f.write("<|endoftext|>".join(train))
+    with open(".temp/eval.txt", "w") as f:
+        f.write("<|endoftext|>".join(eval))
+    # for post in posts:
+    #     emphasize(post.title)
+    #     print(f"\n\n{post.selftext}\n\n")
 
 
 if __name__ == "__main__":
