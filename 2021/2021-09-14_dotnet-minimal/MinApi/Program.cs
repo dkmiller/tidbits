a702d06b90e9156af8b4845f3b110e104d516cd0
Dan Miller <daniel.keegan.miller@gmail.com>
2021-09-15 21:40:03 -0700

    reasonably complete

--- 2021/2021-09-14_dotnet-minimal/MinApi/Program.cs
+++ 2021/2021-09-14_dotnet-minimal/MinApi/Program.cs
@@ -1,8 +1,33 @@
-﻿// using Microsoft.AspNetCore.Builder;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = builder.Environment.ApplicationName, Version = "v1" });
+});
+
 var app = builder.Build();
 
+// Probably only want to do in development environment?
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1"));
+
 app.MapGet("/hello", () => "Hello, World!");
+app.MapGet("/hello/name/{firstName}/{lastName}", (string firstName, string lastName) => $"Hello {firstName} {lastName}");
+
+// Handle request strongly-typed body.
+app.MapPost("/hello", (Person person) => $"Hello, {person.Name} ({person.Age})");
+
+app.MapGet("/age/{age:range(18, 120)}", (int age) => $"You're {age} years old!");
+app.MapGet("/user/{id:guid}", (Guid id) => $"User {id.ToString("X")}");
+
+app.MapGet("/isadult", (int age) => new { IsAdult = age >= 18 });
+
+var logger = app.Logger;
+var config = app.Configuration.GetSection("Config").Get<Config>();
+logger.LogInformation($"Hi from logging (foo = {config.Foo}!");
 
 app.Run();
+
+record Person(string Name, int Age);
+record Config { public string Foo { get; init; } = "default"; }
--- 2021/2021-09-14_dotnet-minimal/README.md
+++ 2021/2021-09-14_dotnet-minimal/README.md
@@ -1,5 +1,38 @@
 # Minimal APIs in .NET 6
 
+Run the local server:
+
+```powershell
+dotnet run --project MinApi
+```
+
+Call the web API:
+
+```powershell
+Invoke-WebRequest http://localhost:5000/hello
+
+iwr  "http://localhost:5000/user/$([guid]::NewGuid())"
+
+# https://stackoverflow.com/a/35725547
+iwr -me post http://localhost:5000/hello -Body '{"name": "dan miller", "age": 29}' -ContentType application/json
+
+iwr http://localhost:5000/isadult?age=19
+```
+
+Format the code:
+
+```
+dotnet format MinApi
+```
+
+[Swagger UI](http://localhost:5000/swagger/).
+
+## To-do
+
+- [ ] Load record-typed configuration from app settings.
+    - [Sadly, only landing in .NET 7](https://github.com/dotnet/runtime/issues/46299)
+- [ ] Authorization (currently getting `No service for type 'Microsoft.AspNetCore.Authentication.IAuthenticationService' has been registered.`)
+
 ## Links
 
 - [Building minimal APIs in .NET 6](https://dotnetcoretutorials.com/2021/07/16/building-minimal-apis-in-net-6/)
