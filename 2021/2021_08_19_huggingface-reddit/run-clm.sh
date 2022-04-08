3197382bf5728aa5c98bbce0d40bbd7d15b4369a
Daniel Miller <daniel.keegan.miller@gmail.com>
2022-04-08 14:46:57 -0700

    Clean up duplicate Huggingface + Reddit directory

--- 2021/2021_08_19_huggingface-reddit/run-clm.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p .temp
-
-if [ ! -f .temp/run_clm.py ]; then
-    # https://stackoverflow.com/a/13735150
-    curl https://raw.githubusercontent.com/huggingface/transformers/master/examples/pytorch/language-modeling/run_clm.py -O
-    mv run_clm.py .temp/run_clm.py
-fi
-
-python .temp/run_clm.py
