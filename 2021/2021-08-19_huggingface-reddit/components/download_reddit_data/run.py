e30585178b1bca3106e1d064ebd097acacd1ddcf
Dan Miller <danmill@microsoft.com>
2022-05-28 15:31:22 +0000

    Cleaned up JSON outputs

--- 2021/2021-08-19_huggingface-reddit/components/download_reddit_data/run.py
+++ 2021/2021-08-19_huggingface-reddit/components/download_reddit_data/run.py
@@ -3,6 +3,7 @@ from azureml.core import Run
 from azureml.core.run import _SubmittedRun
 from dataclasses import dataclass
 from itertools import groupby, count
+import json
 import jsonpickle
 import logging
 import os
@@ -13,6 +14,34 @@ import sys
 
 
 log = logging.getLogger(__name__)
+
+
+def remove_private_keys(o):
+    if not isinstance(o, dict):
+        return
+    public_keys = [k for k in o.keys() if k.startswith("_")]
+    for k in public_keys:
+        del o[k]
+    for v in o.values():
+        remove_private_keys(v)
+
+
+def serialize(post) -> str:
+    """
+    Clean serialization of "raw" Praw submission object. Necessary because
+    existing serialization libraries persist lots of private Praw information,
+    and the Praw team refuses at support access to the "raw" HTTP response.
+
+    WARNING: this is memory intensive, since it (de)serializes the object
+    multiple times.
+
+    https://github.com/praw-dev/praw/issues/830
+    """
+    # https://stackoverflow.com/a/8614096
+    raw_json = jsonpickle.encode(post, unpicklable=False)
+    raw_object = json.loads(raw_json)
+    remove_private_keys(raw_object)
+    return json.dumps(raw_object)
 
 
 @dataclass
@@ -48,9 +77,10 @@ def main(args: Args):
     subreddits = args.subreddits.split(",")
     log.info(f"Downloading data from {len(subreddits)} sub-reddits.")
 
+    # TODO: think about multithreading.
     for subreddit in subreddits:
         log.info(
-            f"Downloading the top '{args.post_limit}' posts from '{subreddit}' by {args.top_mode}"
+            f"Downloading the top {args.post_limit} posts from '{subreddit}' by {args.top_mode}"
         )
         posts = reddit.subreddit(subreddit).top(args.top_mode, limit=args.post_limit)
 
@@ -64,9 +94,8 @@ def main(args: Args):
             # https://stackoverflow.com/a/62348146
             file_path.parent.mkdir(parents=True, exist_ok=True)
             for post in chunk:
-                log.info(f"Writing {subreddit}/{post.id}")
-                # https://stackoverflow.com/a/8614096
-                post_raw = jsonpickle.encode(post)
+                log.debug(f"Writing {subreddit}/{post.id}")
+                post_raw = serialize(post)
                 # https://stackoverflow.com/a/57345569
                 with file_path.open("a") as fp:
                     fp.write(post_raw)
