776f359f0586cf6631e3ba65a52637edbbe7f056
Dan Miller <danmill@microsoft.com>
2022-05-28 14:46:02 +0000

    Use Hydra, convert prepare component to v2

--- 2021/2021-08-19_huggingface-reddit/pipelines/huggingface_reddit.py
+++ 2021/2021-08-19_huggingface-reddit/pipelines/huggingface_reddit.py
@@ -1,31 +1,66 @@
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml import dsl, MLClient, load_component
+import hydra
+import logging
+
+
+log = logging.getLogger(__name__)
 
 
 download_reddit = load_component(
     path="./components/download_reddit_data/component.yaml"
 )
+prepare_json = load_component(path="./components/prepare_json_data/component.yaml")
 
 
-@dsl.pipeline(name="dpv2", default_compute="cpu-cluster")
-def sample_pipeline():
+# https://docs.microsoft.com/en-us/python/api/azure-ai-ml/azure.ai.ml.dsl?view=azure-ml-py
+@dsl.pipeline(default_compute="cpu-cluster", continue_on_step_failure=True)
+def sample_pipeline(
+    client_id: str,
+    client_secret: str,
+    subreddits: str,
+    output_file_name: str,
+    source_jsonpaths: str,
+    source_key: str,
+    target_jsonpath: str,
+    target_key: str,
+):
     reddit_step = download_reddit(
-        client_id="XnkMHEYUujv1wA7EkmToWg",
-        client_secret="secret://reddit-client-secret",
-        subreddits="news,funny",
+        client_id=client_id,
+        client_secret=client_secret,
+        subreddits=subreddits,
     )
     reddit_step.environment_variables = {"AZUREML_COMPUTE_USE_COMMON_RUNTIME": "true"}
 
+    json_step = prepare_json(
+        input_directory=reddit_step.outputs.output_data,
+        output_file_name=output_file_name,
+        source_jsonpaths=source_jsonpaths,
+        source_key=source_key,
+        target_jsonpath=target_jsonpath,
+        target_key=target_key,
+    )
+    json_step.environment_variables = {"AZUREML_COMPUTE_USE_COMMON_RUNTIME": "true"}
 
-p_job = sample_pipeline()
+
+@hydra.main(config_name="config", config_path=".", version_base=None)
+def main(config):
+    for k, level in config.logging.items():
+        logging.getLogger(k).setLevel(level)
+
+    pipeline = sample_pipeline(**config.pipeline)
+    log.info(f"Created pipeline {pipeline}")
+
+    cred = DefaultAzureCredential()
+    ml_client = MLClient(cred, **config.aml)
+
+    job = ml_client.jobs.create_or_update(
+        pipeline, experiment_name="Huggingface-Reddit"
+    )
+
+    job_url = job.services["Studio"].endpoint
+    log.info(f"Submitted {job_url}")
 
 
-cred = DefaultAzureCredential()
-ml_client = MLClient(
-    cred, "48bbc269-ce89-4f6f-9a12-c6f91fcb772d", "aml1p-rg", "aml1p-ml-wus2"
-)
-
-job = ml_client.jobs.create_or_update(p_job, experiment_name="Huggingface-Reddit")
-
-job_url = job.services["Studio"].endpoint
-print(job_url)
+if __name__ == "__main__":
+    main()
