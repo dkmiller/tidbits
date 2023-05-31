546e26f3fb7486670ea834d0a251501a462d654b
Dan Miller <daniel.keegan.miller@gmail.com>
2023-05-31 08:47:51 -0700

    get comments

--- 2023/2023-05_pulls/run.py
+++ 2023/2023-05_pulls/run.py
@@ -15,6 +15,21 @@ async def main(args):
         async for pull in gh.getiter(f"/repos/{args.repo}/pulls?state=all"):
             print(pull["url"])
             x.append(pull)
+            # print(pull)
+            # print("=" * 200)
+            # print(f"/repos/{args.repo}/pulls/{pull['number']}/comments")
+            async for comment in gh.getiter(f"/repos/{args.repo}/pulls/{pull['number']}/comments"):
+                # https://api.github.com/repos/airbnb/lottie-web/pulls/2971/comments
+                print(f"========== comment {comment['id']} >>>>>>>>>>")
+                print(comment["body"])
+                print("-" * 200)
+                print(comment["diff_hunk"])
+                print(f"========== comment {comment['id']} <<<<<<<<<<")
+                print(gh.rate_limit.remaining)
+                # if gf.rate_
+
+            # import sys
+            # sys.exit(0)
         print(len(x))
 
 
