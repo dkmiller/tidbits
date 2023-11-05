a32b3d20908c752468a25cf4c6255e85b9db85bd
Dan Miller <daniel.keegan.miller@gmail.com>
2023-11-04 18:48:39 -0700

    Cache embedding logic

--- 2023/rag/rag/cli.py
+++ 2023/rag/rag/cli.py
@@ -1,13 +1,11 @@
-from sentence_transformers import SentenceTransformer
 import typer
+
+import rag
 
 main = typer.Typer()
 
 
 @main.command()
 def similarity(first: str = "I'm a meat crayon", second: str = "I'm a red crayon"):
-    model = SentenceTransformer("BAAI/bge-large-zh-v1.5")
-    embeddings_1 = model.encode([first], normalize_embeddings=True)
-    embeddings_2 = model.encode([second], normalize_embeddings=True)
-    similarity = embeddings_1 @ embeddings_2.T
-    print(similarity)
+    rv = rag.embedding_similarity([first], [second])
+    print(rv[0][0])
