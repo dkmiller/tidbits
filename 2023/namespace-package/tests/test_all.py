9261bc66d63f00c212be83646e2b206abbc65e09
Dan Miller <daniel.keegan.miller@gmail.com>
2023-11-20 19:37:50 -0800

    Namespace packages in practice (#170)
    * Working but ugly
    
    * Pytest passing
    
    * pip install + pytest working but not intellisense
    
    * Pip install & VS Code both working well
    
    * Looking good

---
+++ 2023/namespace-package/tests/test_all.py
@@ -0,0 +1,16 @@
+def test_all_oryx_core_method():
+    from oryx.core import oryx_core_method
+
+    oryx_core_method()
+
+
+def test_all_oryx_method():
+    from oryx.operations import oryx_method
+
+    oryx_method()
+
+
+def test_all_oryx_cli_main():
+    from oryx.cli import main
+
+    main()
