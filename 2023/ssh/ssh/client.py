54a9641abaa18719b820b2685ff17dc7dbb90e34
Dan Miller <daniel.keegan.miller@gmail.com>
2023-11-21 21:11:40 -0800

    All tests passing

--- 2023/ssh/ssh/client.py
+++ 2023/ssh/ssh/client.py
@@ -10,9 +10,8 @@ class SshCliWrapper:
     identity: Path
     host: SshHost
 
-    def exec(self, *command) -> bytes:
-        args = [
-            "ssh",
+    def _parameters(self):
+        return [
             "-o",
             # https://stackoverflow.com/a/61946687/
             # TODO: relace this with accept-new and/or handle host key checking...
@@ -21,7 +20,42 @@ class SshCliWrapper:
             str(self.identity.absolute()),
             "-p",
             str(self.host.port),
-            f"{self.host.user}@{self.host.host}",
+        ]
+
+    def _hostname(self):
+        return f"{self.host.user}@{self.host.host}"
+
+    def exec(self, *command) -> bytes:
+        args = [
+            "ssh",
+            *self._parameters(),
+            self._hostname(),
             *command,
         ]
         return check_output(args)
+
+    def forward(self, local_port: int, destination_port: int):
+        from subprocess import Popen
+
+        args = [
+            "ssh",
+            *self._parameters(),
+            "-fN",
+            "-L",
+            f"{local_port}:localhost:{destination_port}",
+            self._hostname(),
+        ]
+        return Popen(args)
+        #         ssh -o ConnectTimeout=3 -fN -L $LOCALHOST $TARGET &
+        # return check_output(args)
+
+        # ssh_target = f"{self.user}@{self.host}"
+        # localhost = f"{port}:localhost:{port}"
+        # url = f"http://localhost:{port}/?token={token}"
+        # ssh_tunnel = common.ONE_BRAIN_CLI_ROOT / "one_brain/scripts/ssh_tunnel.sh"
+        # command = f"/bin/bash {ssh_tunnel} {url} {localhost} {ssh_target} {port}"
+
+
+# ssh -L local_port:destination_server_ip:remote_port ssh_server_hostname
+
+# https://phoenixnap.com/kb/ssh-port-forwarding
--- 2023/ssh/tests/test_wrapped_client_and_server.py
+++ 2023/ssh/tests/test_wrapped_client_and_server.py
@@ -1,9 +1,11 @@
 import time
+from uuid import uuid4
+import pytest
 
 from ssh import SshCliWrapper, SshHost, run_dockerized_server
 
 
-def test_client_can_touch_file_in_server(key_pair):
+def test_client_can_call_whoami_in_server(key_pair):
     host = SshHost("localhost", 2222, "dan")
     ssh_client = SshCliWrapper(key_pair["private"], host)
     server = run_dockerized_server(host, key_pair["public"].read_text())
@@ -12,5 +14,61 @@ def test_client_can_touch_file_in_server(key_pair):
     try:
         whoami = ssh_client.exec("whoami")
         assert whoami.decode().strip() == host.user
+    finally:
+        server.stop()
+
+
+def test_client_can_touch_file_in_server(key_pair):
+    host = SshHost("localhost", 2222, "dan")
+    ssh_client = SshCliWrapper(key_pair["private"], host)
+    server = run_dockerized_server(host, key_pair["public"].read_text())
+    time.sleep(3)
+
+    file_name = str(uuid4())
+
+    try:
+        ssh_client.exec("touch", file_name)
+        ls = ssh_client.exec("ls", file_name)
+        assert ls.decode().strip() == file_name
+    finally:
+        server.stop()
+
+
+@pytest.mark.parametrize("executable", ["ls", "nc"])
+def test_client_can_which_executable_in_server(key_pair, executable):
+    host = SshHost("localhost", 2222, "dan")
+    ssh_client = SshCliWrapper(key_pair["private"], host)
+    server = run_dockerized_server(host, key_pair["public"].read_text())
+    time.sleep(2)
+
+    try:
+        which_executable = ssh_client.exec("which", executable)
+        assert which_executable.decode().strip().split("/")[-1] == executable
+    finally:
+        server.stop()
+
+
+def test_client_can_forward_port_from_server(key_pair):
+    host = SshHost("localhost", 2222, "dan")
+    ssh_client = SshCliWrapper(key_pair["private"], host)
+    server = run_dockerized_server(host, key_pair["public"].read_text())
+    time.sleep(2)
+
+    # ssh -L local_port:destination_server_ip:remote_port ssh_server_hostname
+
+    # https://it-tools.tech/random-port-generator
+    local_port = 24464
+    destination_port = 63752
+
+    try:
+        proc = ssh_client.forward(local_port, destination_port)
+        proc.kill()
+
+        # TODO: ssh_client.exec --> "process" variant
+        # ( https://github.com/dkmiller/tidbits/blob/main/2023/kubernetes/src/ptah/core/process.py )
+        # touch,
+        # run nc remotely ( https://unix.stackexchange.com/a/715981 )
+        # (sadly, no python or python3 + no screen)
+        # requests.get from local
     finally:
         server.stop()
