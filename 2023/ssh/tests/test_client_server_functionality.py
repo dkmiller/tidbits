15353bee31ba2b03243a75663bf9985c221ff2dc
Dan Miller <daniel.keegan.miller@gmail.com>
2023-12-03 21:53:21 -0800

    Code is super gross but all tests passing

--- 2023/ssh/tests/test_client_server_functionality.py
+++ 2023/ssh/tests/test_client_server_functionality.py
@@ -27,6 +27,33 @@ def standard(func):
     # TODO: why? You can't have "parameterized fixtures".
     import inspect
 
+    original_sig: inspect.Signature = inspect.signature(func)
+
+    scrubbed_parameters = [
+        p
+        for p in original_sig.parameters.values()
+        if p.name not in ["client", "server"]
+    ]
+
+    injected_params = ["key_pair", "ports", "host", "client_type", "server_type"]
+
+    for name in injected_params:
+        if name not in original_sig.parameters:
+            scrubbed_parameters += [
+                inspect.Parameter(name=name, kind=inspect.Parameter.KEYWORD_ONLY)
+            ]
+
+    sig = original_sig.replace(
+        parameters=scrubbed_parameters
+        # + [
+        #     inspect.Parameter(name="key_pair", kind=inspect.Parameter.KEYWORD_ONLY),
+        #     inspect.Parameter(name="ports", kind=inspect.Parameter.KEYWORD_ONLY),
+        #     inspect.Parameter(name="host", kind=inspect.Parameter.KEYWORD_ONLY),
+        #     inspect.Parameter(name="client_type", kind=inspect.Parameter.KEYWORD_ONLY),
+        #     inspect.Parameter(name="server_type", kind=inspect.Parameter.KEYWORD_ONLY),
+        # ]
+    )
+
     # raise Exception(inspect.signature(func))
     # Cartesian product via parametrize: https://stackoverflow.com/q/22171681/
     @pytest.mark.parametrize("client_type", [FabricClient, SshCliWrapper])
@@ -37,33 +64,27 @@ def standard(func):
     # - https://stackoverflow.com/q/51125966/2543689
     # - https://github.com/pytest-dev/pytest/blob/ef1b91ba8721e85430eead4c2849d8e072fe8350/src/_pytest/python.py#L893
     # - https://github.com/pytest-dev/pytest/blob/ef1b91ba8721e85430eead4c2849d8e072fe8350/src/_pytest/compat.py#L147
-    def wrapper(*args, key_pair, ports, host, client_type, server_type, **kwargs):
-        server = server_type.construct(
-            host, key_pair.public.read_text(), [ports.remote]
+    def wrapper(*args, **kwargs):
+        # key_pair, ports, host, client_type, server_type,
+        server = kwargs["server_type"].construct(
+            kwargs["host"],
+            kwargs["key_pair"].public.read_text(),
+            [kwargs["ports"].remote],
         )
-        client = client_type(key_pair.private, host)
+        client = kwargs["client_type"](kwargs["key_pair"].private, kwargs["host"])
 
         # with server.construct(host, key_pair.public.read_text(), [ports.remote]).serve():
         #     ssh = client(key_pair.private, host)
 
-        return func(*args, client=client, server=server, **kwargs)
+        # raise Exception(f"{injected_params} {list(original_sig.parameters.keys())}")
 
-    sig: inspect.Signature = inspect.signature(func)
+        for name in injected_params:
+            if name not in original_sig.parameters:
+                del kwargs[name]
 
-    scrubbed_parameters = [
-        p for p in sig.parameters.values() if p.name not in ["client", "server"]
-    ]
+        with server.serve():
+            return func(*args, client=client, **kwargs)
 
-    sig = sig.replace(
-        parameters=scrubbed_parameters
-        + [
-            inspect.Parameter(name="key_pair", kind=inspect.Parameter.KEYWORD_ONLY),
-            inspect.Parameter(name="ports", kind=inspect.Parameter.KEYWORD_ONLY),
-            inspect.Parameter(name="host", kind=inspect.Parameter.KEYWORD_ONLY),
-            inspect.Parameter(name="client_type", kind=inspect.Parameter.KEYWORD_ONLY),
-            inspect.Parameter(name="server_type", kind=inspect.Parameter.KEYWORD_ONLY),
-        ]
-    )
     # raise Exception(f"{type(sig)} {sig}")
     wrapper.__signature__ = sig
 
@@ -71,126 +92,114 @@ def standard(func):
 
     # Change function signature: https://stackoverflow.com/a/33112180/
 
+    # https://codereview.stackexchange.com/a/272072
+
     return wrapper
 
 
-# @standard
-# def test_client_can_call_whoami_in_server(client: SshClient):
-#     whoami = client.exec("whoami")
-#     assert whoami.stdout.strip() == client.host.user
+@standard
+def test_client_can_call_whoami_in_server(client: SshClient):
+    whoami = client.exec("whoami")
+    assert whoami.stdout.strip() == client.host.user
 
 
-# @standard
-# def test_client_can_touch_file_in_server(client, server, key_pair, ports, host):
-#     with server(host, key_pair.public, [ports.remote]):
-#         ssh = client(key_pair.private, host)
-#         file_name = str(uuid4())
-#         ssh.exec("touch", file_name)
-#         ls = ssh.exec("ls", file_name)
-#         assert ls.stdout.strip() == file_name
+@standard
+def test_client_can_touch_file_in_server(client: SshClient):
+    file_name = str(uuid4())
+    client.exec("touch", file_name)
+    ls = client.exec("ls", file_name)
+    assert ls.stdout.strip() == file_name
 
 
-# @standard
-# def test_client_can_run_uname_in_server(client, server, key_pair, ports, host):
-#     with server(host, key_pair.public, [ports.remote]):
-#         ssh = client(key_pair.private, host)
-#         uname = ssh.exec("uname", "-a")
-#         prefix = uname.stdout.split()[0]
-#         assert prefix in ["Darwin", "Linux"]
+@standard
+def test_client_can_run_uname_in_server(client: SshClient):
+    uname = client.exec("uname", "-a")
+    prefix = uname.stdout.split()[0]
+    assert prefix in ["Darwin", "Linux"]
 
 
-# @standard
-# def test_client_can_write_to_file_in_server(client, server, key_pair, ports, host):
-#     with server(host, key_pair.public, [ports.remote]):
-#         ssh = client(key_pair.private, host)
-#         file_name = str(uuid4())
-#         file_contents = str(uuid4())
+@standard
+def test_client_can_write_to_file_in_server(client: SshClient):
+    file_name = str(uuid4())
+    file_contents = str(uuid4())
 
-#         ssh.exec("bash", "-c", shlex.quote(f"echo {file_contents} > {file_name}"))
-#         cat_contents = ssh.exec("cat", file_name)
-#         assert cat_contents.stdout.strip() == file_contents
+    client.exec("bash", "-c", shlex.quote(f"echo {file_contents} > {file_name}"))
+    cat_contents = client.exec("cat", file_name)
+    assert cat_contents.stdout.strip() == file_contents
 
 
 @pytest.mark.parametrize(
     "executable", ["bash", "curl", "echo", "ls", "nc", "screen", "wget", "which"]
 )
 @standard
-def test_client_can_run_which_in_server(client, server, executable):
-    with server.serve():
-        which = client.exec("which", executable)
-        assert which.stdout.strip().split("/")[-1] == executable
+def test_client_can_run_which_in_server(client: SshClient, executable):
+    which = client.exec("which", executable)
+    assert which.stdout.strip().split("/")[-1] == executable
 
 
-# @standard
-# def test_client_can_forward_port_from_server(client, server, key_pair, ports, host):
-#     netcat = NetcatClient()
+@standard
+def test_client_can_forward_port_from_server(client: SshClient, ports):
+    netcat = NetcatClient()
 
-#     response_body = f"Hi from SSH server: {uuid4()}"
-#     netcat_command = netcat.ssh_exec(response_body, ports.remote)
+    response_body = f"Hi from SSH server: {uuid4()}"
+    netcat_command = netcat.ssh_exec(response_body, ports.remote)
 
-#     with server(host, key_pair.public, [ports.remote]):
-#         ssh = client(key_pair.private, host)
-#         unescaped_command = " ".join(netcat_command)
-#         log.info("Running %s", unescaped_command)
-#         # https://stackoverflow.com/a/50651839/
-#         ssh.exec(
-#             "screen",
-#             "-S",
-#             "netcat",
-#             "-L",
-#             "-Logfile",
-#             "screen.logs",
-#             "-m",
-#             "-d",
-#             unescaped_command,
-#         )
+    unescaped_command = " ".join(netcat_command)
+    log.info("Running %s", unescaped_command)
+    # https://stackoverflow.com/a/50651839/
+    client.exec(
+        "screen",
+        "-S",
+        "netcat",
+        "-L",
+        "-Logfile",
+        "screen.logs",
+        "-m",
+        "-d",
+        unescaped_command,
+    )
 
-#         assert ".netcat" in ssh.exec("screen -ls").stdout
-#         with ssh.forward(ports.local, remote_port=ports.remote):
-#             response = requests.get(
-#                 f"http://{host.host}:{ports.local}/{uuid4()}", timeout=1
-#             )
+    assert ".netcat" in client.exec("screen -ls").stdout
+    with client.forward(ports.local, remote_port=ports.remote):
+        response = requests.get(
+            f"http://{client.host.host}:{ports.local}/{uuid4()}", timeout=1
+        )
 
-#         response.raise_for_status()
-#         # TODO: why is the "strip" required?
-#         assert response.text.strip() == response_body.strip()
-#         request = response.request
+    response.raise_for_status()
+    # TODO: why is the "strip" required?
+    assert response.text.strip() == response_body.strip()
+    request = response.request
 
-#         netcat_logs = ssh.exec("cat", "screen.logs").stdout
+    netcat_logs = client.exec("cat", "screen.logs").stdout
 
-#         assert all(
-#             f"{name}: {value}" in netcat_logs for name, value in request.headers.items()
-#         )
+    assert all(
+        f"{name}: {value}" in netcat_logs for name, value in request.headers.items()
+    )
 
-#         assert f"{request.method} {request.path_url}" in netcat_logs
+    assert f"{request.method} {request.path_url}" in netcat_logs
 
 
-# @standard
-# def test_remote_screen_session_with_netcat_and_curl(
-#     client, server, key_pair, ports, host
-# ):
-#     """
-#     Connect local -> remote server. Start a screen session with netcat exposed on a specified port
-#     remotely. Curl that screen session remotely.
+@standard
+def test_remote_screen_session_with_netcat_and_curl(client: SshClient, ports):
+    """
+    Connect local -> remote server. Start a screen session with netcat exposed on a specified port
+    remotely. Curl that screen session remotely.
 
-#     (All this needed to ensure remote netcat server is behaving properly, before bringing port
-#     forwarding into the mix.)
-#     """
-#     netcat = NetcatClient()
+    (All this needed to ensure remote netcat server is behaving properly, before bringing port
+    forwarding into the mix.)
+    """
+    netcat = NetcatClient()
 
-#     response_body = f"Hi from SSH server: {uuid4()}"
-#     netcat_command = netcat.ssh_exec(response_body, ports.remote)
+    response_body = f"Hi from SSH server: {uuid4()}"
+    netcat_command = netcat.ssh_exec(response_body, ports.remote)
 
-#     with server(host, key_pair.public, [ports.remote]):
-#         ssh = client(key_pair.private, host)
-#         with ssh.forward(ports.local, remote_port=ports.remote):
-#             unescaped_command = " ".join(netcat_command)
-#             log.info("Running %s", unescaped_command)
-#             ssh.exec("screen", "-S", "netcat", "-m", "-d", unescaped_command)
+    unescaped_command = " ".join(netcat_command)
+    log.info("Running %s", unescaped_command)
+    client.exec("screen", "-S", "netcat", "-m", "-d", unescaped_command)
 
-#             assert ".netcat" in ssh.exec("screen", "-ls").stdout
+    assert ".netcat" in client.exec("screen", "-ls").stdout
 
-#             curl = ssh.exec("curl", "-v", f"http://{host.host}:{ports.remote}/path")
-#             assert curl.status == 0
-#             assert response_body in curl.stdout
-#             assert "Excess found in a read" not in curl.stderr
+    curl = client.exec("curl", "-v", f"http://{client.host.host}:{ports.remote}/path")
+    assert curl.status == 0
+    assert response_body in curl.stdout
+    assert "Excess found in a read" not in curl.stderr
