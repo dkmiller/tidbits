f9ae12bf56eedc3885261dd7ee21674056ec177b
Dan Miller <daniel.keegan.miller@gmail.com>
2024-01-17 20:23:38 -0800

    Add some more links

--- 2023/kubernetes/examples/e2e/images/orleans/Program.cs
+++ 2023/kubernetes/examples/e2e/images/orleans/Program.cs
@@ -1,1 +1,4 @@
 System.Console.WriteLine("Hi from inside Docker!");
+
+// TODO: https://learn.microsoft.com/en-us/dotnet/orleans/deployment/kubernetes
+// https://learn.microsoft.com/en-us/dotnet/orleans/tutorials-and-samples/tutorial-1
---
+++ 2023/kubernetes/examples/e2e/images/orleans/README.md
@@ -0,0 +1,9 @@
+# Orleans
+
+https://formulae.brew.sh/cask/dotnet-sdk
+
+https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit
+
+```bash
+dotnet run
+```
