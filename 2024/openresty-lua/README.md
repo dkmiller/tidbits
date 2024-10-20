a7c99456dd6b4c719fbf5d65e2d1693dc220660e
Dan Miller <dan.miller@airbnb.com>
2024-10-20 07:34:19 -0700

    Working

---
+++ 2024/openresty-lua/README.md
@@ -0,0 +1,9 @@
+# OpenResty + Lua
+
+## Run
+
+First `./run.sh`, then any of the commands below.
+
+```bash
+curl localhost:8080
+```
--- 2024/openresty-lua/run.sh
+++ 2024/openresty-lua/run.sh
@@ -1,1 +1,3 @@
 docker build -t proxy .
+
+docker run --volume $PWD/work:/srv -p 8080:8080 proxy /bin/bash -c "nginx -p /srv/ -c conf/nginx.conf; sleep infinity"
