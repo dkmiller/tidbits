008dce703300257445131055c15343dfd3932c2e
Dan Miller <daniel.keegan.miller@gmail.com>
2025-05-10 08:49:44 -0700

    Use OpenAI

--- 2025/braintrust-otel/README.md
+++ 2025/braintrust-otel/README.md
@@ -8,12 +8,16 @@ pip install -r requirements.txt
 fastapi dev
 
 curl -H "content-type: application/json" localhost:8000/chat -d '{"name": "dan"}'
+
+ruff check --fix . && ruff format .
 ```
 
 - https://www.braintrust.dev/docs/guides/traces/integrations#opentelemetry-otel
 - https://fastapi.tiangolo.com/advanced/events/#lifespan-function
 - https://opentelemetry.io/docs/languages/python/instrumentation/
 - https://opentelemetry.io/docs/languages/python/exporters/#usage
+- https://www.braintrust.dev/app/dkmiller/p/dkmiller-first-project/
+- https://github.com/openai/openai-python?tab=readme-ov-file#async-usage
 
 TODO
 
--- 2025/braintrust-otel/main.py
+++ 2025/braintrust-otel/main.py
@@ -2,7 +2,8 @@ from contextlib import asynccontextmanager
 from dataclasses import dataclass
 
 from dotenv import dotenv_values
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from openai import AsyncOpenAI
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -17,16 +18,20 @@ class ChatRequest:
     name: str
 
 
+def dotenv() -> dict:
+    return dotenv_values()
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     OpenAIInstrumentor().instrument()
 
-    env = dotenv_values()
-    api_key = env["braintrust_api_key"]
-    project_id = env["braintrust_project_id"]
+    # It is not straightforward to leverage FastAPI dependencies within the lifespan:
+    # https://github.com/fastapi/fastapi/discussions/11742
+    env = dotenv()
     headers = {
-        "Authorization": f"Bearer {api_key}",
-        "x-bt-parent": f"project_id:{project_id}",
+        "Authorization": f"Bearer {env['braintrust_api_key']}",
+        "x-bt-parent": f"project_id:{env['braintrust_project_id']}",
     }
     print(f"OTel headers = {headers}")
 
@@ -43,12 +48,27 @@ async def lifespan(app: FastAPI):
     yield
 
 
+def openai(env: dict = Depends(dotenv)) -> AsyncOpenAI:
+    return AsyncOpenAI(api_key=env["openai_api_key"])
+
+
 app = FastAPI(debug=True, lifespan=lifespan)
 
 
 @app.post("/chat")
-def chat(req: ChatRequest):
-    return f"hi, {req.name}"
+async def chat(req: ChatRequest, openai: AsyncOpenAI = Depends(openai)):
+    # The OTel instrumentation SDK for OpenAI isn't responses-aware yet.
+    response = await openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "developer", "content": "Politely great the user by name"},
+            {
+                "role": "user",
+                "content": f"I'm {req.name}",
+            },
+        ],
+    )
+    return response.choices[0].message.content
 
 
 # Cannot add middleware after the application has started.
