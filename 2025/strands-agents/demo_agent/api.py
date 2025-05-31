90157527dcb537ff9866bed2d615b5443dfc2987
Dan Miller <daniel.keegan.miller@gmail.com>
2025-05-31 10:29:28 -0700

    Strands agents (#215)
    * Slow as f*** but works
    
    * Traces -> Honeycomb
    
    * REST + HTTPie
    
    * Fully migrate to FastAPI
    
    * FastAPI instrumentation
    
    * FastMCP
    
    * TODOs & links

---
+++ 2025/strands-agents/demo_agent/api.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from fastapi import Depends, FastAPI
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from strands import Agent
+
+from demo_agent.dependencies import primes_agent, tracer
+
+
+@dataclass
+class PromptRequest:
+    prompt: str
+
+
+app = FastAPI(title="Strands agents demo")
+
+
+# TODO: how to get structured agent outputs?
+@app.post("/primes_agent")
+def call_primes_agent(
+    request: PromptRequest,
+    agent: Agent = Depends(primes_agent),
+    # Forcibly enable tracing. TODO: do this via middleware etc.?
+    _=Depends(tracer),
+):
+    response = agent(prompt=request.prompt)
+    return {"result": str(response)}
+
+
+FastAPIInstrumentor.instrument_app(app)
