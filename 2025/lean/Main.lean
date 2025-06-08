f44a2b5aad467b09d4b925d66d2c8ba5a9157d38
Dan Miller <daniel.keegan.miller@gmail.com>
2025-06-08 16:49:01 -0700

    Git attributes + generics

--- 2025/lean/Main.lean
+++ 2025/lean/Main.lean
@@ -1,5 +1,12 @@
 import Src.Basic
 
+def triple :=
+  λ (n: Nat) => 3 * n
+
+-- A kind of generics, nice!
+def multiplyFuncs (α β: Type) (g: α → Nat) (h: β → Nat) :=
+  λ (a: α) (b: β) => g a * h b
+
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
 
@@ -11,3 +18,8 @@ def main : IO Unit :=
   def a: α := b
   #check a
   #check Type
+  #check List
+  def f: (Nat → Nat) :=  λ (x: Nat) => x * x
+  #check f
+  #check λ (u: Nat → Nat)(v: Nat → Nat)(x: Nat) => u x * v x
+  #eval triple 7
