ed626096164fd6600fef2abefb7ab9537b7afd8e
Dan Miller <daniel.keegan.miller@gmail.com>
2025-07-22 14:40:16 -0700

    Repro Braintrust :: OTel issues (#223)
    * Repro Braintrust :: OTel issues
    
    * Fix names

--- 2025/braintrust-propagation/otel.py
+++ 2025/braintrust-propagation/otel.py
@@ -4,7 +4,8 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+
 
 
 def opentelemetry_init(*, project_id=None, experiment_id=None):
@@ -26,4 +27,6 @@ def opentelemetry_init(*, project_id=None, experiment_id=None):
     tracer_provider = TracerProvider(resource=resource)
     processor = BatchSpanProcessor(OTLPSpanExporter(headers=headers))
     tracer_provider.add_span_processor(processor)
+    console_processor = BatchSpanProcessor(ConsoleSpanExporter())
+    tracer_provider.add_span_processor(console_processor)
     trace.set_tracer_provider(tracer_provider)
