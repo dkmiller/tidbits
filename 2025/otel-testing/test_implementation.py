e2a5f45af9948dca540a3278658a75732f10b6b2
Dan Miller <dan.miller@airbnb.com>
2025-07-30 06:17:45 -0700

    Demonstrate this resets each time...

--- 2025/otel-testing/test_implementation.py
+++ 2025/otel-testing/test_implementation.py
@@ -1,19 +1,19 @@
-from opentelemetry import trace
+from opentelemetry.trace import StatusCode
 
 from implementation import failing_method, some_method
+
+
+def test_failing_method(captrace):
+    failing_method()
+
+    assert StatusCode.ERROR in [s.status.status_code for s in captrace.spans]
 
 
 def test_method(captrace):
     some_method()
 
     assert set(["inner-span", "some-span"]) <= set(s.name for s in captrace.spans)
-    assert trace.StatusCode.ERROR not in [s.status.status_code for s in captrace.spans]
+    assert StatusCode.ERROR not in [s.status.status_code for s in captrace.spans]
 
     inner_span = [s for s in captrace.spans if s.name == "inner-span"][0]
     assert [e.name for e in inner_span.events] == ["First event", "Second event"]
-
-
-def test_failing_method(captrace):
-    failing_method()
-
-    assert trace.StatusCode.ERROR in [s.status.status_code for s in captrace.spans]
