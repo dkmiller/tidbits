4a8465692e54efab2eb3b354df5da04d8a122003
Dan Miller <dan.miller@airbnb.com>
2026-02-02 08:40:11 -0800

    progress...

--- 2026/greedy-santa/greedy_santa/__init__.py
+++ 2026/greedy-santa/greedy_santa/__init__.py
@@ -1,5 +1,4 @@
 from typing import Callable, TypeVar
-from unittest.mock import MagicMock
 
 import polars as pl
 
@@ -13,28 +12,66 @@ class RowTracer:
         # self.expressions: dict[str, pl.Expr] = {}
 
     def __getitem__(self, key):
-        if self.identity is None:
+        if self.wrapped is None:
             # Get or return a "Tracer" object whose identity is $row[key]
             # self.expressions[f"row[{key}]"] = pl.col(key)
-            return RowTracer(identity=pl.col(key))
+            return RowTracer(wrapped=pl.col(key))
         else:
-            raise NotImplementedError()
-        
+            if isinstance(key, RowTracer):
+                raise NotImplementedError("getitem")
+            else:
+                if isinstance(key, int):
+                    # TODO: we should have a more reliable way of detecting "list"-type.
+                    return RowTracer(wrapped=self.wrapped.list.get(key))
+                else:
+                    raise NotImplementedError("getitem")
+
+    # TODO: how many of
+    # https://docs.pola.rs/api/python/dev/reference/expressions/api/polars.Expr.str.to_integer.html
+    # can we do?
+
     def __bool__(self):
         raise NotImplementedError("bool")
         # TODO: if encountering "conversion to truthy", fork:
         # - one path "if true", another "if false".
 
+    def __add__(self, right):
+        if isinstance(right, RowTracer):
+            return RowTracer(self.wrapped + right.wrapped)
+        else:
+            return NotImplementedError("add")
+
+    def __int__(self):
+        # TODO: replace builtins?
+        # https://stackoverflow.com/a/61042819
+        return RowTracer(self.wrapped.str.to_integer())
+
+    def __sub__(self, right):
+        if isinstance(right, RowTracer):
+            return RowTracer(self.wrapped - right.wrapped)
+
+    def __mul__(self, right):
+        if isinstance(right, RowTracer):
+            return RowTracer(self.wrapped * right.wrapped)
+
+    def split(self, sep: str | None = None):
+        return RowTracer(self.wrapped.str.split(by=sep))
+
+        # pl.col("variable").str.split(by="_").list.get(1).alias("row"),
+
+
+
     # TODO: all of
     # https://www.pythonmorsels.com/every-dunder-method/#cheat-sheet
 
-def compile[T](callable: Callable[[dict], T]) -> pl.Expr:
-    fake_row = RowTracer()
-    fake_result = callable(fake_row)
 
-    return pl.col("c")
+def compile[T](callable: Callable[[dict], T]) -> pl.Expr:
+    fake_row = RowTracer(None)
+    fake_result = callable(fake_row)
+    return fake_result.wrapped
 
 
 def initialize():
     # TODO: add a new "map_elements" method that takes a lambda.
     # What about `return_dtype`? Can we infer it now?
+    pass
--- 2026/greedy-santa/tests/test_greedy_santa.py
+++ 2026/greedy-santa/tests/test_greedy_santa.py
@@ -4,10 +4,17 @@ import polars as pl
 from greedy_santa import compile
 
 
-@pytest.mark.parametrize("callable,expected", [
-    (lambda row: row["a"] + row["b"], pl.col("a") + pl.col("b")),
-    # (lambda row: "hi" if row else "bye", pl.col("boo")),
-])
+@pytest.mark.parametrize(
+    "callable,expected",
+    [
+        (lambda row: row["a"] + row["b"], pl.col("a") + pl.col("b")),
+        (lambda row: row["c"] - row["d"], pl.col("c") - pl.col("d")),
+        (lambda row: row["e"] * row["f"], pl.col("e") * pl.col("f")),
+        (lambda row: row["g"].split(":")[0], pl.col("g").str.split(by=":").list.get(0)),
+        # (lambda row: int(row["h"]), pl.col("h").str.to_integer),
+        (lambda row: "hi" if row else "bye", pl.col("boo")),
+    ],
+)
 def test_compile(callable, expected):
     compiled = compile(callable)
     assert compiled.meta.serialize() == expected.meta.serialize()
