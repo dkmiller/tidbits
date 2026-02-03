c0a0bce2ab5c5fefe11ca4cd3e342c2a2f990c34
Dan Miller <dan.miller@airbnb.com>
2026-02-02 22:39:42 -0800

    more natural operators

--- 2026/greedy-santa/tests/test_greedy_santa.py
+++ 2026/greedy-santa/tests/test_greedy_santa.py
@@ -14,12 +14,15 @@ from greedy_santa import compile
         (lambda row: row["e"] * row["f"], pl.col("e") * pl.col("f")),
         (lambda row: row["g"].split(":")[0], pl.col("g").str.split(by=":").list.get(0)),
         (lambda row: row["age"] > 28, pl.col("age") > 28),
+        (lambda row: row["a"] >= 1, pl.col("a").ge(1)),
+        (lambda row: 1 > row["a"], 1 > pl.col("a")),
+        (lambda row: 1 >= row["a"], 1 >= pl.col("a")),
         (lambda row: row["left"] / 2, pl.col("left") / 2),
         (lambda row: row["left"] / row["right"], pl.col("left") / pl.col("right")),
         (lambda row: 1 / row["right"], 1 / pl.col("right")),
         (lambda row: row["a"] ** row["b"], pl.col("a").pow(pl.col("b"))),
         (lambda row: 2 ** row["b"], 2 ** pl.col("b")),
-        (lambda row: row["a"] or row["b"], pl.col("a").or_(pl.col("b"))),
+        (lambda row: -row["b"], -pl.col("b")),
         # (lambda row: int(row["h"]), pl.col("h").str.to_integer),
     ],
 )
@@ -38,6 +41,7 @@ def _for_loop(row):
     [
         lambda row: "hi" if row["h"] == "val" else "bye",
         _for_loop,
+        lambda row: row["a"] or row["b"],
     ],
 )
 def test_unsupported(callable):
